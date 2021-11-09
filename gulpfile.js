1e34d5c213ad4ef7f197f23b5e918f06a6ae73f9
Alex <alex@2a03.party>
2021-11-09 20:47:09 +0000

    API Docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 /* eslint-disable */
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const typedoc = require("gulp-typedoc");
 
 const tsProject = ts.createProject('tsconfig.json');
 gulp.task('default', function() {
     return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('out'));
+});
+
+gulp.task("docs", function() {
+    return gulp
+        .src(["src/*.ts"])
+        .pipe(typedoc({
+            // Output options (see TypeDoc docs http://typedoc.org/api/interfaces/typedocoptionmap.html)
+            // NOTE: the out option and the json option cannot share the same directory
+            out: "./docs/api/html/",
+            json: "./docs/api/file.json",
+
+            // TypeDoc options (see TypeDoc docs http://typedoc.org/api/interfaces/typedocoptionmap.html)
+            name: "Dutch: Time Manager",
+            version: true,
+        }));
 });
